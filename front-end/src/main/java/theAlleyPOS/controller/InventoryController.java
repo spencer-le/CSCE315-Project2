6e39837a901568b8a382db0b1c0580b7eac0747e
Sebastian Oberg <Sebastianoberg2002@tamu.edu>
2023-10-11 11:04:21 -0500

    Added inventory screen

--- front-end/src/main/java/theAlleyPOS/controller/InventoryController.java
+++ front-end/src/main/java/theAlleyPOS/controller/InventoryController.java
@@ -1,4 +1,14 @@
 package theAlleyPOS.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class InventoryController {
+    @FXML
+    public void handleAddItem(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    public void handleUpdateItem(ActionEvent actionEvent) {
+    }
 }

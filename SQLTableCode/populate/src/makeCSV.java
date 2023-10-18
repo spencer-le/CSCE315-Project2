5977ac68f5da91a7b86870e0bbf1eb67bed45df9
gshields432 <75458960+gshields432@users.noreply.github.com>
2023-10-17 20:54:38 -0500

    PopulateDatabase function in DatabaseHelper.java

--- SQLTableCode/populate/src/makeCSV.java
+++ SQLTableCode/populate/src/makeCSV.java
@@ -32,7 +32,28 @@ public class makeCSV {
         func();
     }
     static void func(){
-        String[] drink_arr = new String[20];
+        String[] drink_arr = {"Brown Sugar Deerioca Fresh Milk",
+        "Cocoa Brown Sugar Deerioca Fresh Milk",
+        "Matcha Brown Sugar Deerioca Fresh Milk",
+        "Snow Strawberry Lulu",
+        "Orange Lulu",
+        "Snow Velvet Peach Oolong Tea",
+        "Snow Velvet Brown Sugar Latte",
+        "Hojicha Peach Oolong Latte",
+        "Matcha Peach Oolong Latte",
+        "Royal No.9 Tea",
+        "Assam Black Tea",
+        "Peach Oolong Tea",
+        "Royal No.9 Milk Tea",
+        "Assam Milk Tea",
+        "Garden Milk Tea",
+        "Northern Lights"}; //16 drinks 0-15
+        // non drink items needing to be included
+        // "Morning Dawn"
+        //        "Napkins"
+        //        "Bags"
+        //        "Cup"
+        //        "Straws"
         String[] ice_arr   = {"light ice", "regular ice", "extra ice"};
         String[] sugar_arr = {"light sugar", "regular sugar", "extra sugar"};
         int drink_ind = 0;
@@ -43,15 +64,10 @@ public class makeCSV {
         int purchase_id = 0;
         FileWriter myWriter = null;
 
-        //initialize Drink names:
-        for (int i = 0; i < 20; i++) {
-            drink_arr[i] = "Drink" + i;
-        }
-
 
         // creating a file [from w3schools]
         try {
-            File myObj = new File("year_data.csv");
+            File myObj = new File("year_data_new.csv");
 
             // this is so a new file actually gets made
             if (myObj.exists()){
@@ -66,34 +82,35 @@ public class makeCSV {
                 System.out.println("Failed to create file");
             }
 
-            myWriter = new FileWriter("year_data.csv", true); // true for append mode
+            myWriter = new FileWriter("purchases_data.csv", true); // true for append mode
         } catch (IOException e) {
             System.out.println("An error occurred.");
         }
-
-
-
-        for (int weeks = 0; weeks < 365; weeks++){
+        int customer_name_counter = -1;
+        for (int days = 1; days <= 365; days++){
             weekly_customer_count = (int) (Math.random() * 901) + 100; //each week generates a random customer count
 
             while(weekly_customer_count > 0){//and for each customer there are a random number of purchases.
                 // NOTE: inside this loop is an order
                 customer_purchases = (int) (Math.random() * 5) + 1;
-
-                //there are a random number of purchases
-                customer_purchases = (int) (Math.random() * 5) + 1;
-
+                customer_name_counter++; // each order is only placed by one customer, hence we update the counter once here
                 while(customer_purchases > 0){
+                    //inside this loop is a single purchase
                     //ints to access arr items at indices
-                    drink_ind = (int) (Math.random() * 20); // int between 0 and 19 (inclusive)
+                    drink_ind = (int) (Math.random() * 16); // int between 0 and 15 (inclusive)
                     ice_ind = (int) (Math.random() * 3);    // Generates a random number between 0 and 2 (inclusive)
                     sugar_ind = (int) (Math.random() * 3);  // Generates a random number between 0 and 2 (inclusive)
-
                     try {
+                        // current table setup
+                        // id, customer_name, order_date, total_cost, hours
+                        // new table setup
+                        // id, customer_name, order_date, total_cost
+
+
                         myWriter.write(
-                                purchase_id + "," + weeks + "," + drink_arr[drink_ind] + "," + ice_arr[ice_ind] + "," +
-                                        sugar_arr[sugar_ind] + ",\n");
-                        // Don't close the FileWriter here
+                                purchase_id + "," + "Customer" + customer_name_counter + "," + days + "," + drink_arr[drink_ind] + "," + ice_arr[ice_ind] + "," +
+                                        sugar_arr[sugar_ind] + ","+ (int)(Math.random() * 11.0) + 11,\n");
+
                     } catch (IOException e) {
                         System.out.println("An error occurred.");
                     }
--- front-end/src/main/java/theAlleyPOS/model/DatabaseHelper.java
+++ front-end/src/main/java/theAlleyPOS/model/DatabaseHelper.java
@@ -4,13 +4,19 @@ import theAlleyPOS.model.Item;
 import theAlleyPOS.model.Order;
 import theAlleyPOS.model.Modifier;
 import theAlleyPOS.model.UserRole;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class DatabaseHelper {
 
@@ -21,7 +27,194 @@ public class DatabaseHelper {
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, USER, PASS);
     }
+    private void PopulateDatabase(){
+    /*
+    brainstorm
 
+    Need (write to 2 places)
+    (Order) --> a list of all orders placed
+        Id, customer_name, order_date, total_cost, bags(bool), napkins(bool)
+    (Ordered_items) --> a list of all items (drinks and auxiliary) ordered
+        Id, item, modifiers
+
+    Year loop (365 day)
+        each day has random (20-100) customers
+        EXCEPT day 3 and 7 (peak days)
+            customers are set at 150
+
+        Customer Loop (runs for each customer)
+            double order_total = 0; // reset each time
+            bool bag = false;
+            bool napkins = false;
+            int drinks_ordered: each customer orders random (1-6) drinks
+            if (drinks_ordered > 3){
+                bag = true;
+            }
+            random (1-5)
+                if 1, napkins = true;
+
+            Drink Loop (Runs drinks_ordered times)
+
+                each drink has a chance of modifiers. reset with each loop
+                string modifiers = ""
+                    modifiers append to the modifiers string
+                ice_modifier: random 0-4:
+                    0: less ice
+                    1: more ice
+                    2-4: default ice
+                sweet_modifier: random 0-4:
+                    0: less sugar
+                    1: more sugar
+                    2-4: default sugar
+                drink_choice: random 0-16:
+                    0 Brown Sugar Deerioca Fresh Milk
+                    1 Cocoa Brown Sugar Deerioca Fresh Milk
+                    2 Matcha Brown Sugar Deerioca Fresh Milk
+                    3 Snow Strawberry Lulu
+                    4 Orange Lulu
+                    5 Snow Velvet Peach Oolong Tea
+                    6 Snow Velvet Brown Sugar Latte
+                    7 Hojicha Peach Oolong Latte
+                    8 Matcha Peach Oolong Latte
+                    9 Royal No.9 Tea
+                    10 Assam Black Tea
+                    11 Peach Oolong Tea
+                    12 Royal No.9 Milk Tea
+                    13 Assam Milk Tea
+                    14 Garden Milk Tea
+                    15 Northern Lights
+                    16 Morning Dawn
+                add price of drink to total
+                write to ordered_items csv: order_id, drink_choice, modifiers, \n
+             End Drink Loop
+             write to orders csv: order_id, customer_name, order_date, order_total, bags, napkins
+             increment order_id
+         End Customer Loop
+     End Year loop
+     */
+        Random random = new Random();
+        FileWriter myWriter_order = null;
+        FileWriter myWriter_ordered_items = null;
+
+        try { //create file for order table
+            File myObj = new File("order_year_data.csv");
+            if (myObj.exists() && myObj.delete()) {
+                System.out.println("File deleted: " + myObj.getName());
+            }
+
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("Failed to create file");
+            }
+
+            myWriter_order = new FileWriter("order_year_data.csv", true);
+            myWriter_order.write("Id, customer_name, order_date, total_cost, bags, napkins\n");
+        } catch (IOException var12) {
+            System.out.println("An error occurred.");
+        }
+
+        try { //create file for ordered_items table
+            File myObj = new File("order_year_data.csv");
+            if (myObj.exists() && myObj.delete()) {
+                System.out.println("File deleted: " + myObj.getName());
+            }
+
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("Failed to create file");
+            }
+
+            myWriter_order = new FileWriter("ordered_items_year_data.csv", true);
+            myWriter_order.write("Id, item, modifiers\n");
+        } catch (IOException var12) {
+            System.out.println("An error occurred.");
+        }
+
+        int order_id = 0;
+        int customers;
+        for(int days = 0; days < 365; days++){ //days loop - determines day of order
+            // Generate a random number between 20 and 100
+            customers = random.nextInt((100 - 20 + 1) + 20);
+            //rand.nextInt((max - min) + 1) + min;
+            if(days == 3 || days == 7){
+                customers = 150;
+            }
+            while(customers > 0){ // customers loop - one for each customer
+                double order_total = 0; // reset for each customer
+                boolean bag = false;
+                boolean napkins = false;
+                int drinks_ordered = random.nextInt(6) + 1;// each customer orders random (1-6) drinks
+                // setting the time variable
+                LocalDateTime base_time = LocalDateTime.now().minusDays(365-days);
+                // Generate a random number of hours and minutes
+                int randomHours = random.nextInt(24); // Random hours between 0 and 23
+                int randomMinutes = random.nextInt(60); // Random minutes between 0 and 59
+
+                // Add the random hours and minutes to the base date-time
+                LocalDateTime order_time = base_time.plusHours(randomHours).plusMinutes(randomMinutes);
+                if(drinks_ordered > 3){
+                    bag = true; // larger orders require a bag
+                }
+                int napkin_rand = random.nextInt(5) + 1;
+                if(napkin_rand == 1){
+                    napkins = true; //random chance of needing a napkin
+                }
+                while(drinks_ordered > 0){ // drink loop
+                    String modifiers = ""; //will store ice/sweetness modifiers
+                    int ice_modifier = random.nextInt(5);
+                    if(ice_modifier == 0){
+                        modifiers += "less ice";
+                    }
+                    if(ice_modifier == 1){
+                        modifiers += "more ice";
+                    }
+                    int sweet_modifier = random.nextInt(6);
+                    if(sweet_modifier == 0){
+                        modifiers += " less sugar";
+                    }
+                    if(sweet_modifier == 1){
+                        modifiers += " more sugar";
+                    }
+                    int drink_choice = random.nextInt(17); // 0-16
+                    double drink_price = 0;
+                    String sql = "SELECT price FROM items WHERE id = " + drink_choice;
+                    try (Connection conn = getConnection(); //this code block grabs the price from the drink table
+                         PreparedStatement pstmt = conn.prepareStatement(sql)) {
+                        try (ResultSet rs = pstmt.executeQuery()) {
+                            if(rs.next()){
+                                drink_price = rs.getDouble("price");
+                            }
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                    order_total += drink_price; // adds total for order required in customer_loop
+                    try {
+                        myWriter_ordered_items.write("" + order_id + "," + drink_choice + "," + modifiers + "\n");
+                    } catch (IOException var11) {
+                        System.out.println("An error occurred.");
+                    }
+
+                    drinks_ordered--;
+                } // END OF DRINK LOOP
+
+                // remainder of customer loop is writing to file with total
+
+                try {
+                    //write to orders csv: order_id, customer_name, order_date, order_total, bags, napkins
+                    myWriter_order.write("" + order_id + ",Customer " + order_id + "," + order_time + "," + order_total
+                            + "," + bag + "," + napkins + "\n");
+                } catch (IOException var11) {
+                    System.out.println("An error occurred.");
+                }
+                customers--;
+                order_id++; //order is complete
+            } // END OF CUSTOMERS LOOP
+        }
+
+    }
     public UserRole validateUser(String ID) {
         if (isIDValid(ID, "Managers")) {
             return UserRole.MANAGER;

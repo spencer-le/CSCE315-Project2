aecdf114f6732d0d5e3a7da440f624b92df23e0a
gshields432 <75458960+gshields432@users.noreply.github.com>
2023-10-17 21:21:48 -0500

    currently bug fixing. the function runs, its just that the csv files dont have any data in them.

--- front-end/src/main/java/theAlleyPOS/model/DatabaseHelper.java
+++ front-end/src/main/java/theAlleyPOS/model/DatabaseHelper.java
@@ -27,7 +27,8 @@ public class DatabaseHelper {
     private Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, USER, PASS);
     }
-    private void PopulateDatabase(){
+    public void PopulateDatabase(){
+
     /*
     brainstorm
 
@@ -108,14 +109,14 @@ public class DatabaseHelper {
                 System.out.println("Failed to create file");
             }
 
-            myWriter_order = new FileWriter("order_year_data.csv", true);
+            myWriter_order = new FileWriter("order_year_data.csv");
             myWriter_order.write("Id, customer_name, order_date, total_cost, bags, napkins\n");
         } catch (IOException var12) {
             System.out.println("An error occurred.");
         }
 
         try { //create file for ordered_items table
-            File myObj = new File("order_year_data.csv");
+            File myObj = new File("ordered_items_year_data.csv");
             if (myObj.exists() && myObj.delete()) {
                 System.out.println("File deleted: " + myObj.getName());
             }
@@ -126,7 +127,7 @@ public class DatabaseHelper {
                 System.out.println("Failed to create file");
             }
 
-            myWriter_order = new FileWriter("ordered_items_year_data.csv", true);
+            myWriter_order = new FileWriter("ordered_items_year_data.csv");
             myWriter_order.write("Id, item, modifiers\n");
         } catch (IOException var12) {
             System.out.println("An error occurred.");
@@ -192,7 +193,8 @@ public class DatabaseHelper {
                     }
                     order_total += drink_price; // adds total for order required in customer_loop
                     try {
-                        myWriter_ordered_items.write("" + order_id + "," + drink_choice + "," + modifiers + "\n");
+                        myWriter_ordered_items = new FileWriter("ordered_items_year_data.csv");
+                        myWriter_ordered_items.write(order_id + "," + drink_choice + "," + modifiers + "\n");
                     } catch (IOException var11) {
                         System.out.println("An error occurred.");
                     }
@@ -204,7 +206,8 @@ public class DatabaseHelper {
 
                 try {
                     //write to orders csv: order_id, customer_name, order_date, order_total, bags, napkins
-                    myWriter_order.write("" + order_id + ",Customer " + order_id + "," + order_time + "," + order_total
+                    myWriter_order = new FileWriter("order_year_data.csv");
+                    myWriter_order.write(order_id + ",Customer " + order_id + "," + order_time + "," + order_total
                             + "," + bag + "," + napkins + "\n");
                 } catch (IOException var11) {
                     System.out.println("An error occurred.");
@@ -212,6 +215,21 @@ public class DatabaseHelper {
                 customers--;
                 order_id++; //order is complete
             } // END OF CUSTOMERS LOOP
+        }
+        try {
+            if (myWriter_ordered_items != null) {
+                myWriter_ordered_items.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (myWriter_order != null) {
+                myWriter_order.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
     }
